d2eec40128b1d7fa9fedf65d680363baf2d27b55
Wilkins Morales <wilkinscom@gmail.com>
2023-01-19 23:28:04 +0200

    "improved index and started mainjs"

--- js/main.js
+++ js/main.js
@@ -1,21 +1,56 @@
-const myGameArea = {
-    canvas: document.createElement('canvas'),
-    start: function (){
-        this.canvas.width = 720;
-        this.canvas.height = 400;
-        this.context = this.canvas.getContext('2d');
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    }
-}
+// init important variables
+let canvas,ctx,loop;
+let ball,player1;
 
-const startGame = () =>{
-    console.log('Game is starting! ')
+// define canvas and context
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d');
+
+
+function startGame() {
+    console.log('Game is starting! ');
+    let start = document.getElementById('game-start')
+    let game = document.getElementById('game-area')
+    let over = document.getElementById('game-over')
     //diplay none for start game screen
     //display "" for game-area
+    start.style.display = 'none'
+    game.style.display = 'block'
+    over.style.display = 'none'
+
+    // function to start the game
+    start();
 }
 
-const stopGame = () => {
-    console.log('Game is stopped!')
-     //display none for game area
-     //display "" for game over screen
+loop = setInterval(() => {
+    update();
+    render();
+}, 1000/60);
+
+function init (){
+
+}
+
+function update() {
+    console.log('this will update')
+}
+
+function render(){
+    console.log('rendering')
+}
+
+function stopGame() {
+    console.log('Game is stopped!');
+
+    start.style.display = 'none'
+    game.style.display = 'none'
+    over.style.display = 'block'
+
+    document.getElementById('game-area').style.display = 'none';
+    document.getElementById('game-over').style.display = '';
+    //display none for game area
+    //display "" for game over screen
+
+    // reset values here
+    // clear interval
 }
